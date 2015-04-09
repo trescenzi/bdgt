7c323af040e65b3dee2b5265b6e0e75fcf0ce5d1
trescenzi <trescenzi@gmail.com>
2015-04-09 01:56:46 +0000

    this has got to be the least safe thing ever

--- serve.js
+++ serve.js
@@ -1,13 +1,24 @@
 var express = require('express');
 var path = require( 'path' );
 var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var _ = require('lodash');
 
 app.use( express.static( 'node_modules' ) );
 app.use( express.static( 'dist' ) );
 app.use( express.static( 'src' ) );
+app.use(bodyParser.json());
 
 app.get( '/', function( req, res ) {
   res.sendFile( path.join( __dirname + '/index.html' ) );
+});
+
+app.put('/fake_data.json', function(req, res) {
+  _.map(req.body, function( item ) {
+    delete item.$$hashKey;
+  });
+  fs.writeFile('dist/fake_data.json', JSON.stringify(req.body, null, 2));
 });
 
 
--- src/bdgt.controller.js
+++ src/bdgt.controller.js
@@ -1,37 +1,46 @@
-angular.module('bdgt').controller( 'BDController', (function() {
-  function BDController() {
-    this.exampleItems = [
-      { name: 'paycheck', value: 1000 },
-      { name: 'dinner', value: -25 },
-      { name: 'rent', value: -500 }
-    ];
-  }
-
-  BDController.prototype = {
-    constructor: BDController,
-    spentThisMonth: function() {
-     return _.foldl( this.exampleItems, function( sum, item ) {
+angular.module('bdgt').controller( 'BDController', function( $http ) {
+  
+    this.spentThisMonth = function(){
+     return _.foldl( this.data, function( sum, item ) {
         if(item.value < 0) {
           return sum + item.value;
         }
         return sum;
       }, 0);
-    },
-    addItem: function(name, value) {
-      this.exampleItems.push({'name': name, 'value':value});
-    },
-    earnedThisMonth: function() {
-     return _.foldl( this.exampleItems, function( sum, item ) {
+    };
+
+    this.addItem = function(name, value) {
+      this.data.push({'name': name, 'value':parseFloat(value)});
+      this.saveData();
+    };
+
+    this.earnedThisMonth = function() {
+        console.log( 'earned', this.data);
+     return _.foldl( this.data, function( sum, item ) {
+        console.log(item);
         if(item.value > 0) {
           return sum + item.value;
         }
         return sum;
       }, 0);
-    },
-    total: function() {
-      return this.earnedThisMonth() + this.spentThisMonth();
-    }
-  }
+    };
 
-  return BDController;
-}()));
+    this.total = function() {
+      return this.earnedThisMonth() + this.spentThisMonth();
+    };
+
+    this.getBudgetItems = function() {
+      console.log( 'calling function' );
+      $http.get('fake_data.json').then(_.bind(this.setData, this));
+    };
+
+    this.setData = function( response ) {
+      this.data = response.data;
+    };
+
+    this.saveData = function() {
+      console.log('putting',this.data, JSON.stringify(this.data));
+      $http.put('fake_data.json', JSON.stringify(this.data));
+    }
+
+});
